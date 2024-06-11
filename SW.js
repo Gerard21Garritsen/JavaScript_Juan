85bbd18ba7baa9d370ba59e7a09d8cd9ac3a80fd
Garrix <gilbertarch47@gmail.com>
2024-06-11 02:19:44 -0600

    Service Worker enable

---
+++ SW.js
@@ -0,0 +1,8 @@
+//This file has all code related with Servive Worker
+
+self.addEventListener("install", e => console.log(e));
+
+self.addEventListener("activate", e => console.log(e));
+
+self.addEventListener("fetch", e => console.log(e));
+
---
+++ js/app.js
@@ -0,0 +1,14 @@
+//This file has all code in order to get PWA
+if('serviceWorker' in navigator)
+{
+    navigator.serviceWorker.register("/47-ServiceWorkers-PWA/SW.js")
+    .then(registro => console.log("SW registrado", registro))
+    .catch(error => console.log(error));
+    
+}
+else
+{
+    console.log("El navegador no soporta SW");
+
+}
+
