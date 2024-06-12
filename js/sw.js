624100eb440b7033035e2ee0b72c7c314065a140
Garrix <gilbertarch47@gmail.com>
2024-06-12 01:01:39 -0600

    Add again cache

--- js/sw.js
+++ js/sw.js
@@ -1,16 +1,16 @@
 //This file has all code related with Servive Worker
 
-// const cache_name = "apv-v1";
+const cache_name = "apv-v1";
 
-// const datos_cache = [
+const datos_cache = [
     
-//     "/47-ServiceWorkers-PWA/index.html",
-//     "/47-ServiceWorkers-PWA/error.html",
-//     "/47-ServiceWorkers-PWA/css/bootstrap.css",
-//     "/47-ServiceWorkers-PWA/css/styles.css",
-//     "/47-ServiceWorkers-PWA/js/app.js",
-//     "/47-ServiceWorkers-PWA/js/apv.js"
-// ]
+    "/47-ServiceWorkers-PWA/index.html",
+    "/47-ServiceWorkers-PWA/error.html",
+    "/47-ServiceWorkers-PWA/css/bootstrap.css",
+    "/47-ServiceWorkers-PWA/css/styles.css",
+    "/47-ServiceWorkers-PWA/js/app.js",
+    "/47-ServiceWorkers-PWA/js/apv.js"
+]
 
 
 self.addEventListener("install", e =>
@@ -18,10 +18,10 @@ self.addEventListener("install", e =>
     console.log("Installing...", e);
 
     //adding to the cache
-    // e.waitUntil(
-    //     caches.open(cache_name)
-    //     .then(cache => cache.addAll(datos_cache))
-    // )
+    e.waitUntil(
+        caches.open(cache_name)
+        .then(cache => cache.addAll(datos_cache))
+    )
 
 });
 
@@ -34,11 +34,11 @@ self.addEventListener("fetch", e =>
     console.log(e)
     
     //get cache data
-    // e.respondWith(
-    //     caches.match(e.request)
-    //     .then(result => result)
-    //     .catch(() => caches.match("/47-ServiceWorkers-PWA/error.html"))
-    // )
+    e.respondWith(
+        caches.match(e.request)
+        .then(result => result)
+        .catch(() => caches.match("/47-ServiceWorkers-PWA/error.html"))
+    )
     
 });
 
